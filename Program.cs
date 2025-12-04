15c12e7590d8048b302d510302449b479980266c
Nabeel Khan <nabeelk0799@gamil.com>
2025-12-04 14:55:57 +0300

    Inital Commit

--- Program.cs
+++ Program.cs
@@ -14,7 +14,9 @@ namespace oxoid_web_scrapring
 
 
             // Goal of this project: 
-                // 1. 
+                // 1. Use Iron Pdf to extract CAT no's and store in db/csv
+                // 2. Connect with sfda website to extract license status.
+                // 3. Update status in db/csv
             Console.WriteLine("hello this is my first line of code in a million years.");
 
 
