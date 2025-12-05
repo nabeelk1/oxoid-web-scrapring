d05e3755636a9f3eda2c83d4ed3699d3a69c5353
Nabeel Khan <nabeelk0799@gamil.com>
2025-12-05 19:09:51 +0300

    Added UglyToad.PdfPig to extract text from PDF.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+
 namespace oxoid_web_scrapring
 {
     internal class Program
@@ -18,6 +23,18 @@ namespace oxoid_web_scrapring
                 // 2. Connect with sfda website to extract license status.
                 // 3. Update status in db/csv
             Console.WriteLine("hello this is my first line of code in a million years.");
+
+            using (PdfDocument document = PdfDocument.Open(@"C:\Users\nabeelk\Home\Al-Majharia\oxoid-project\scraper\oxoid-web-scrapring\2025-EU-Microbiology-Catalog_EN.pdf"))
+            {
+                foreach (Page page in document.GetPages())
+                {
+                    string text = ContentOrderTextExtractor.GetText(page);
+                    IEnumerable<Word> words = page.GetWords(NearestNeighbourWordExtractor.Instance);
+
+                    Console.WriteLine($"####Page Number{page.Number}###");
+                    Console.WriteLine($"{text}");
+                }
+            }
 
 
 
